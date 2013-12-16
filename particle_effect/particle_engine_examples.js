4ded989bbad98757050e6548ef19e5cb04afbfaf
junaidk <joni630@gmail.com>
2013-12-16 15:00:25 +0500

    Smoke and shadows

--- particle_effect/particle_engine_examples.js
+++ particle_effect/particle_engine_examples.js
@@ -123,6 +123,33 @@ Examples =
         {
                 positionStyle    : Type.CUBE,
                 positionBase     : new THREE.Vector3( 0, 0, 0 ),
+                positionSpread   : new THREE.Vector3( 5, 0, 5 ),
+
+                velocityStyle    : Type.CUBE,
+                velocityBase     : new THREE.Vector3( 0, 150, 0 ),
+                velocitySpread   : new THREE.Vector3( 70, 40, 70 ), 
+                accelerationBase : new THREE.Vector3( 0,-10,0 ),
+                
+                particleTexture : THREE.ImageUtils.loadTexture( 'img/fire.png'),
+
+                angleBase               : 0,
+                angleSpread             : 100,
+                angleVelocityBase       : 0,
+                angleVelocitySpread     : 100,
+                
+                sizeTween    : new Tween( [0, 1], [10, 38] ),
+                opacityTween : new Tween( [0.8, 2], [0.5, 0] ),
+                colorTween   : new Tween( [0.4, 1], [ new THREE.Vector3(0,0,0.2), new THREE.Vector3(0, 0, 0.5) ] ),
+
+                particlesPerSecond : 120,
+                particleDeathAge   : 2.0,                
+                emitterDeathAge    : 40
+        },
+
+        smokeReal :
+        {
+                positionStyle    : Type.CUBE,
+                positionBase     : new THREE.Vector3( 0, 0, 0 ),
                 positionSpread   : new THREE.Vector3( 10, 0, 10 ),
 
                 velocityStyle    : Type.CUBE,
